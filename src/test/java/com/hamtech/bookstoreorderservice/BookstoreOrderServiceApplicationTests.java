fe883cef67237b97eb952fd0c1ca5d32bc65ba22
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 23:00:14 +0700

    init

---
+++ src/test/java/com/hamtech/bookstoreorderservice/BookstoreOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstoreorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
